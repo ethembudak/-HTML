af1bc559cdc0a9f6503d80d39a550194ac424c44
Ethem Budak <fatihethem@gmail.com>
2017-04-23 12:00:38 +0300

    Default Homepage

---
+++ shared/footer.php
@@ -0,0 +1,7 @@
+    <!-- JQuery 1.12.4 -->
+    <script src="assets/javascripts/jquery.min.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="assets/project.min.js"></script>
+  </body>
+</html>
---
+++ widget/search.php
@@ -0,0 +1,3 @@
+<form class="navbar-form navbar-right">
+  <input type="text" class="form-control" placeholder="Search...">
+</form>
